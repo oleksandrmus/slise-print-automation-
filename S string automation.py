3d9974d7a91d94ddb1c3c7f6eec8918f6ca6b9f1
oleksandrmus <84258179+oleksandrmus@users.noreply.github.com>
2021-09-22 11:16:40 -0400

    Add files via upload

---
+++ S string automation.py
@@ -0,0 +1,8 @@
+my_string = "test automation "
+print(my_string[5:15])
+print(my_string[-11:])
+print(my_string[:5])
+print(my_string[-16:-11])
+my_list=[10,50,20,25,30]
+print(my_list[1:3])
+print(my_list[-4:-2])
